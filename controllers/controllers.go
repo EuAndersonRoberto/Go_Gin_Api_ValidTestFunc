2900abda5fec91ca8fd06a3bad90d46f0d3f8349
EuAndersonRoberto <euandersondigital@gmail.com>
2022-12-25 10:58:01 -0300

    Implemeting templates and routes

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -76,3 +76,10 @@ func BuscaAlunoPorCPF(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, aluno)
 }
+
+func ExibePaginaIndex(c *gin.Context) {
+	c.HTML(http.StatusOK, "index,html", gin.H{
+		"mensagem": "Bem-vindos(as)!",
+	})
+
+}
--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,7 @@ import (
 
 func HandleRequests() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
 	r.GET("/alunos", controllers.ExibeTodosAlunos)          // Aqui definimos que toda requisão GET feita para o endpoint: /alunos, irá responder com a função "ExibeTodosAlunos", nos devolvendo o código 200 e a mensagem JSON: "id":"1" e "nome":"Anderson Roberto", na URL: http://localhost:8080/alunos
 	r.GET("/:nome", controllers.Saudacao)                   //Aqui definimos mais uma requisição GET, sendo específicado o endpoint "/:nome" que tem a possibilidade de ser alterado e o responsável por essa requisição GET é o controllers."Saudacao".
 	r.POST("/alunos", controllers.CriaNovoAluno)            //Aqui definimos uma requisição POST, responsável por criar um novo aluno no endpoint "/alunos".
@@ -14,5 +15,6 @@ func HandleRequests() {
 	r.DELETE("/alunos/:id", controllers.DeleteAluno)        //Aqui definimos uma requisição DELETE, específicando que dentro do endpoint "/alunos/:id" e tendo o controllers."DeleteAluno" como responsável por essa requisição.
 	r.PATCH("/alunos/:id", controllers.AtualizaAluno)       //Aqui definimos uma requisição UPDATE, específicando que dentro do endpoint "/alunos/:id" e tendo o controllers."AtualizaAluno" como responsável por essa requisição.
 	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF) //Aqui definimos uma requisição GET, específicando um novo endpoint "/alunos/cpf/:cpf" e tendo o controllers."BuscaAlunoPorCPF" como responsável por essa requisição.
+	r.GET("/index", controllers.ExibePaginaIndex)           //Aqui definimos uma requisição GET, específicando um novo endpoint "/index" e tendo o controllers."ExibePaginaIndex" como responsável por essa requisição.
 	r.Run()
 }
